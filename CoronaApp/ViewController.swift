3450062ec69f2e73f193fda142bbf354c0bd17b3
kentomiyazawa <kento4241@yahoo.co.jp>
2020-06-30 23:45:34 +0900

    Initial Commit

---
+++ CoronaApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoronaApp
+//
+//  Created by 宮沢建人 on 2020/06/30.
+//  Copyright © 2020 miyazawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
